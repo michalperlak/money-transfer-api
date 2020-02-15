720fc89c0618dbb7d1835b8f563482b961b3f3e8
Michal Perlak <michalperlak@windowslive.com>
2020-02-15 13:03:18 +0100

    Implement class representing money value

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,14 @@ repositories {
 
 val arrowVersion: String by project
 val junitVersion: String by project
+val equalsVerifierVersion: String by project
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("io.arrow-kt", "arrow-core", arrowVersion)
 
     testImplementation("org.junit.jupiter", "junit-jupiter", junitVersion)
+    testImplementation("nl.jqno.equalsverifier", "equalsverifier", equalsVerifierVersion)
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/pl/michalperlak/moneytransfer/core/domain/Money.kt
@@ -0,0 +1,46 @@
+package pl.michalperlak.moneytransfer.core.domain
+
+import java.math.BigDecimal
+import java.math.RoundingMode.HALF_EVEN
+import java.util.*
+
+class Money private constructor(
+        private val value: BigDecimal
+) {
+    fun asString(): String = value.toPlainString()
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null || this.javaClass != other.javaClass) {
+            return false
+        }
+        val that = other as Money
+        return value == that.value
+    }
+
+    override fun hashCode(): Int = Objects.hash(value)
+
+    operator fun compareTo(other: Money): Int {
+        return value.compareTo(other.value)
+    }
+
+    operator fun plus(other: Money): Money {
+        return Money(value + other.value)
+    }
+
+    operator fun minus(other: Money): Money {
+        if (value < other.value) {
+            throw IllegalArgumentException("Cannot subtract greater value. Current value: $value, subtracted: ${other.value}")
+        }
+        return Money(value - other.value)
+    }
+
+    companion object {
+        fun of(value: String) = of(BigDecimal(value))
+
+        fun of(value: Long): Money = of(BigDecimal.valueOf(value))
+
+        fun of(value: BigDecimal): Money = Money(value.setScale(2, HALF_EVEN))
+
+        val ZERO = Money(BigDecimal.ZERO.setScale(2, HALF_EVEN))
+    }
+}
