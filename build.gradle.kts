7b4cdef88dbb96043d333ef2a49d605d1bb2948a
Michal Perlak <michalperlak@windowslive.com>
2020-02-15 12:11:10 +0100

    Initial project structure

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "pl.michalperlak"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val arrowVersion: String by project
+val junitVersion: String by project
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.arrow-kt", "arrow-core", arrowVersion)
+
+    testImplementation("org.junit.jupiter", "junit-jupiter", junitVersion)
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
